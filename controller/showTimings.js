fb9faade6eba18230a669b80eafaa3b4d2599264
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-07-29 14:21:02 +0530

    show timings updated

--- controller/showTimings.js
+++ controller/showTimings.js
@@ -1,46 +1,29 @@
-const route = require('express').Router();
-const show = require('../model/shows');
+const route = require('express').Router()
+const show = require('../model/shows')
 const moment = require('moment'); 
+
 
 route.post('/showTimings', (req, res) => {
   console.log(req.body);
   const { title, showDate } = req.body;
-  console.log(title, showDate);
-  
-  const currentDate = new Date();
-  if (req.body.showDate === currentDate.toISOString().split('T')[0]) {
-    show.find({
-      title: title,
-      showDate: showDate
-    })
-    .then((shows) => {
-      console.log(shows);
-      const currentTime = moment();
-      const filteredShows = shows.map((show) => {
-        const givenDateTime = moment(show.showTimes, 'hh:mm A');
-        return {
-          ...show,
-          showTimes: givenDateTime.isAfter(currentTime) ? show.showTimes : null
-        };
-      });
-      const validShows = filteredShows.filter(show => show.showTimes !== null);
-
-      if (validShows.length === 0) {
-        res.sendStatus(404);
-      } else {
-        console.log("filteredShows", validShows);
-        res.send(validShows);
-      }
-      })
-      .catch(err => {
-      console.log(err);
-      res.sendStatus(404);
-    });
-  } else {
-    let errorObj = {};
-    errorObj['error'] = "Invalid date";
-    res.sendStatus(503).send(errorObj);
-  }
+  console.log(title,showDate);
+  show.find({
+    title: title,
+    showDate: showDate
+  })
+  .then((shows) => {
+       if(shows.length === 0){
+          res.sendStatus(404);
+       }
+       else{
+          res.send(shows);
+       }
+  })
+  .catch(err => {
+    console.log(err);
+    res.sendStatus(404);
+  });
 });
+
 
 module.exports = route;
