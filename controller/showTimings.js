b45fc8a4328a0245f809b934a8c44dc7cd479acf
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-07-29 13:55:27 +0530

    show timings updated

--- controller/showTimings.js
+++ controller/showTimings.js
@@ -1,12 +1,11 @@
-const route = require('express').Router()
-const show = require('../model/shows')
+const route = require('express').Router();
+const show = require('../model/shows');
 const moment = require('moment'); 
-
 
 route.post('/showTimings', (req, res) => {
   console.log(req.body);
   const { title, showDate } = req.body;
-  console.log(title,showDate);
+  console.log(title, showDate);
   
   const currentDate = new Date();
   if (req.body.showDate === currentDate.toISOString().split('T')[0]) {
@@ -15,32 +14,33 @@ route.post('/showTimings', (req, res) => {
       showDate: showDate
     })
     .then((shows) => {
-        console.log(shows);
-        const currentTime = moment();
-        const filteredShows = shows.filter((show) => {
-          const givenDateTime = moment(show.showTimes, 'hh:mm A', true);
-          console.log("Bla bla" , givenDateTime,currentTime);
-          return currentTime.isAfter(givenDateTime);
-        });
-        if(filteredShows.length == 0){
-            res.sendStatus(404);
-        }
-        else{
-          console.log("filteredShows", filteredShows);
-          res.json(filteredShows);
-        }
+      console.log(shows);
+      const currentTime = moment();
+      const filteredShows = shows.map((show) => {
+        const givenDateTime = moment(show.showTimes, 'hh:mm A', true);
+        return {
+          ...show,
+          showTimes: givenDateTime.isAfter(currentTime) ? show.showTimes : null
+        };
+      });
+      const validShows = filteredShows.filter(show => show.showTimes !== null);
+
+      if (validShows.length === 0) {
+        res.sendStatus(404);
+      } else {
+        console.log("filteredShows", validShows);
+        res.json(validShows);
+      }
     })
     .catch(err => {
       console.log(err);
       res.sendStatus(404);
     });
-  }
-  else{
-    let errorObj={};
-    errorObj['error']="Invalid date";
-    res.statusCode(503);
+  } else {
+    let errorObj = {};
+    errorObj['error'] = "Invalid date";
+    res.status(503).json(errorObj);
   }
 });
-
 
 module.exports = route;
