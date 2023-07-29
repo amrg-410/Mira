adcf35c5d4e7da4e53409768ba02c347925a0e13
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-07-29 13:45:49 +0530

    showTimings updated

--- controller/showTimings.js
+++ controller/showTimings.js
@@ -7,36 +7,39 @@ route.post('/showTimings', (req, res) => {
   console.log(req.body);
   const { title, showDate } = req.body;
   console.log(title,showDate);
-  show.find({
-    title: title,
-    showDate: showDate
-  })
-  .then((shows) => {
-    const currentDate = new Date();
-    if (req.body.showDate === currentDate.toISOString().split('T')[0]) {
-      console.log(shows);
-      const currentTime = moment();
-      const filteredShows = shows.filter((show) => {
-          for(var i=0;i<shows.length;i++){
-            const givenDateTime = moment(show[i].showTimes, 'hh:mm A', true);
-            return givenDateTime.isAfter(currentTime);
-          }
-      });
-      if(filteredShows.length == 0){
-          res.sendStatus(404);
-      }
-      else{
-        console.log("filteredShows", filteredShows);
-        res.json(filteredShows);
-      }
-    } else {
-      res.json(shows);
-    }
-  })
-  .catch(err => {
-    console.log(err);
-    res.sendStatus(404);
-  });
+  
+  const currentDate = new Date();
+  if (req.body.showDate === currentDate.toISOString().split('T')[0]) {
+    show.find({
+      title: title,
+      showDate: showDate
+    })
+    .then((shows) => {
+        console.log(shows);
+        const currentTime = moment();
+        const filteredShows = shows.filter((show) => {
+          const givenDateTime = moment(show.showTimes, 'hh:mm A', true);
+          console.log(givenDateTime);
+          return givenDateTime.isAfter(currentTime);
+        });
+        if(filteredShows.length == 0){
+            res.sendStatus(404);
+        }
+        else{
+          console.log("filteredShows", filteredShows);
+          res.json(filteredShows);
+        }
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(404);
+    });
+  }
+  else{
+    let errorObj={};
+    errorObj['error']="Invalid date";
+    res.statusCode(503);
+  }
 });
 
 
