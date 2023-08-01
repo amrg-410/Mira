57255d86a0b26cae4c9ec61962a52b47561f9dc0
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-08-01 21:06:27 +0530

    show time update

--- controller/showTimings.js
+++ controller/showTimings.js
@@ -15,7 +15,41 @@ route.post("/showTimings", (req, res) => {
           res.sendStatus(404);
        }
        else{
-          res.send(shows);
+        let responseData={};
+          for(var i=0;i<shows.length;i++){
+            function getCurrentTimeInAMPM() {
+              const date = new Date();
+              const hours = date.getHours();
+              const minutes = date.getMinutes();
+              const seconds = date.getSeconds();
+              const amPM = hours >= 12 ? 'PM' : 'AM';
+              const formattedHours = (hours % 12) || 12;
+              const formattedMinutes = String(minutes).padStart(2, '0');
+              const formattedSeconds = String(seconds).padStart(2, '0');
+              const currentTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${amPM}`;
+              return currentTime;
+            }
+            function isTimePassed(givenTime) {
+              const currentDate = new Date();
+              const givenTimeParts = givenTime.split(':');
+              const givenHours = parseInt(givenTimeParts[0]);
+              const givenMinutes = parseInt(givenTimeParts[1].split(' ')[0]);
+              const isPM = givenTimeParts[1].toLowerCase().includes('pm');
+              const hours24Format = (givenHours % 12) + (isPM ? 12 : 0);
+              const givenTimeDate = new Date();
+              givenTimeDate.setHours(hours24Format, givenMinutes, 0, 0);
+              return currentDate > givenTimeDate;
+            }
+            const currentTime = getCurrentTimeInAMPM(); 
+            const givenTime = shows[i].showTimes;
+            const isPassed = isTimePassed(givenTime);
+            
+            if (isPassed) {
+               responseData+=shows[i];
+            } else {
+              console.log(`${givenTime} is yet to come.`);
+            }
+          }
        }
   })
   .catch(err => {
