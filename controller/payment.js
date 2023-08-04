a52019c8a4fac52cfcd2d06ee8d9acc107414ac4
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-08-04 12:25:41 +0530

    payment delay updated

--- controller/payment.js
+++ controller/payment.js
@@ -3,7 +3,7 @@ const route = require('express').Router()
 route.get('/delay', (req, res) => {
     setTimeout(() => {
       res.send('Response after 20 seconds delay');
-    }, 20000); 
+    }, 5000); 
   });
 
 module.exports = route
