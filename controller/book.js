53572e5189a0750663cee6f1101310bbb0764557
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-08-04 21:28:16 +0530

    booking updated

--- controller/book.js
+++ controller/book.js
@@ -9,7 +9,7 @@ const generateBookingId = () => {
 };
 
 route.post('/book', (req, res) => {
-  const { title, showDate, theater, showTime, seats, user } = req.body;
+  const { title, showDate, theater, showTime, seats, user, paymentId } = req.body;
   console.log(req.body);
   const bookingId = generateBookingId();
   const newBooking = new Booking({
