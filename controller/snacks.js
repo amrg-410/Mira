5155d0008a6724b6b5a0a020b2968fe2d90825bc
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-08-06 14:34:43 +0530

    route changed

--- controller/snacks.js
+++ controller/snacks.js
@@ -2,7 +2,14 @@ const route = require('express').Router()
 const snacks = require('../model/snacks')
 
 route.post('/fetchSnacks',(req,res)=>{
-    res.send(snacks)
+    snacks.find()
+    .then((snack)=>{
+        res.send(snack)
+    })
+    .catch((error) => {
+        console.error('Error in snacks :', error);
+        res.status(500).json({ error: 'Snacks error.' });
+    });
 })
 
 
