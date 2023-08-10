575571bc3833d366b3901e78bf1c2c86507f0106
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-08-10 09:20:20 +0530

    model changes

--- model/shows.js
+++ model/shows.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
   theater: { type: String, required: true },
-  showDate: { type: Date, required: true ,unique: true},
+  showDate: { type: Date, required: true },
   showTimes: { type: String, required: true },
   ticketPrice: { type: Number, required: true },
   seatAvailability: { type: Number, required: true },
