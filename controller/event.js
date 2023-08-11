8ba56536754a620e5a57da38681110733c42ae99
amrg-410 <rahulgandiaids@smvec.ac.in>
2023-08-11 21:12:54 +0530

    event mail added

--- controller/event.js
+++ controller/event.js
@@ -1,5 +1,22 @@
 const route = require('express').Router()
 const events = require('../model/event');
+const nodemailer=require("nodemailer")  
+const handlebars = require("handlebars")
+const fs = require("fs")
+const path = require("path")
+const emailTemplateSource = fs.readFileSync(path.join(__dirname, "/tempEvent.hbs"), "utf8") 
+
+
+let transporter = {
+    service: 'gmail',
+    auth: {
+    user: 'chatbotmira0@gmail.com' , 
+    pass: 'sqnm cylz knbp ukma', 
+    }
+};
+
+const smtpTransport = nodemailer.createTransport(transporter)
+const template = handlebars.compile(emailTemplateSource)
 
 
 route.post('/saveEvent',(req,res)=>{
@@ -8,6 +25,33 @@ route.post('/saveEvent',(req,res)=>{
     console.log("Event Added");
     res.sendStatus(200);
 })
+
+
+route.post("/eventMail", (req, res) => {
+    const {email,name,phone,date} = req.body;
+    console.log(req.body);
+      const htmlToSend = template({ 
+        name:name,
+        phone:phone,
+        date:date
+      });
+      const mailOptions = {
+          from: 'chatbotmira0@gmail.com',
+          to: email,
+          subject: 'Hosting Event Details',
+          html: htmlToSend
+      };
+      smtpTransport.sendMail(mailOptions)
+          .then((info) => {
+              res.sendStatus(200);
+          })
+          .catch((err) => {
+              console.log('gmail');
+              console.log(err);
+              res.sendStatus(400);
+          });
+    
+});
 
 
 module.exports=route
